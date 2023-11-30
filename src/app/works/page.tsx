91645c6490c2c83c1cc7063fe7e6376c51d98c16
Wz <dzinlife@me.com>
2023-11-30 18:13:35 +0800

    cache image optimize

--- src/app/works/page.tsx
+++ src/app/works/page.tsx
@@ -4,8 +4,7 @@ import LoadingPage from "@/components/LoadingPage";
 
 export const runtime = "edge";
 
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
+export const revalidate = 300;
 
 const Works: React.FC = async () => {
   return (
